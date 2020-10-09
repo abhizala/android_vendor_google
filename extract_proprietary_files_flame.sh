dbcb7c402893effa7388b4f0c36b099c0b991345
Craig Gomez <craigacgomez@gmail.com>
2020-10-08 20:07:39 -0700

    Flame: Squashed commit to update proprietary files for Android 11 (rp1a.201005.004)

--- extract_proprietary_files_flame.sh
+++ extract_proprietary_files_flame.sh
@@ -16,29 +16,38 @@ copy () {
 echo -e "\e[1mExtracting 'system' proprietary files\e[0m"
 cat $PWD/flame/proprietary-files-system.txt | while read proprietary_file
 do
-    source_location=/media/_/system
+    source_location=/mnt/factory-image/flame/system/system
     target_location=$PWD/flame/proprietary
     copy $proprietary_file $source_location $target_location
 done
 
+echo -e "\e[1mExtracting 'system_ext' proprietary files\e[0m"
+cat $PWD/flame/proprietary-files-system_ext.txt | while read proprietary_file
+do
+    source_location=/mnt/factory-image/flame/system_ext
+    target_location=$PWD/flame/proprietary/system_ext
+    copy $proprietary_file $source_location $target_location
+done
+echo -e "\e[1mFixing invalid 'system_ext' proprietary xml files\e[0m"
+echo -e "\t\e[32mFixing file system_ext/etc/permissions/telephonyservice.xml\e[0m"
+sed -i 's/\/system\/system_ext/\/system_ext/' $PWD/flame/proprietary/system_ext/etc/permissions/telephonyservice.xml
+
 echo -e "\e[1mExtracting 'product' proprietary files\e[0m"
 cat $PWD/flame/proprietary-files-product.txt | while read proprietary_file
 do
-    source_location=/media/product
+    source_location=/mnt/factory-image/flame/product
     target_location=$PWD/flame/proprietary/product
     copy $proprietary_file $source_location $target_location
 done
 
 echo -e "\e[1mFixing invalid 'product' proprietary xml files\e[0m"
-echo -e "\t\e[32mFixing file product/etc/permissions/telephonyservice.xml\e[0m"
-sed -i 's/\/system\/product/\/product/' $PWD/flame/proprietary/product/etc/permissions/telephonyservice.xml
 echo -e "\t\e[32mFixing file product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml\e[0m"
 sed -i 's/version="2.0"/version="1.0"/' $PWD/flame/proprietary/product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
 
 echo -e "\e[1mExtracting 'vendor' proprietary files\e[0m"
 cat $PWD/flame/proprietary-files-vendor.txt | while read proprietary_file
 do
-    source_location=/media/vendor
+    source_location=/mnt/factory-image/flame/vendor
     target_location=$PWD/flame/proprietary/vendor
     copy $proprietary_file $source_location $target_location
 done
